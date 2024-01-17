f5212d577f20aa28b4338445ee39b7167ba67a7e
erikajdavid <123127113+erikajdavid@users.noreply.github.com>
2024-01-17 12:55:03 -0500

    refacrtored code

--- server/middlewear/authorization.js
+++ server/middlewear/authorization.js
@@ -1,12 +1,15 @@
 const jwt = require("jsonwebtoken");
 
 const auth = async (req, res, next) => {
-    try {
-        const jwtToken = req.header("token");
+    const jwtToken = req.header("token");
+
+    console.log(jwtToken);
 
         if (!jwtToken) {
             return res.status(403).json({ message: `Unauthorized.` })
         }
+    
+        try {
 
         const payload = jwt.verify(jwtToken, process.env.JWT_SECRET);
 
