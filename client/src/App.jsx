20b780015c470ecf93e37ef55bc32a21eda7db8a
erikajdavid <123127113+erikajdavid@users.noreply.github.com>
2024-01-17 19:42:27 -0500

    updated path for dashboard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Routes, Route, Navigate } from 'react-router-dom';
-import Dashboard from './components/Dashboard';
+import Dashboard from './components//dashboard/Dashboard';
 import Login from './components/Login';
 import Register from './components/Register';
 
